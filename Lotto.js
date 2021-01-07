210dfb1847c463f4c83a2f3a94dce68c69bb2726
Kiryeon <sol920601@gmail.com>
2021-01-07 22:14:41 +0900

    edit again

--- Lotto.js
+++ Lotto.js
@@ -2,12 +2,15 @@
 function searching(search_text){
     $.ajax({ // Request
     method: "GET",
-    url: "https://dapi.kakao.com/v2/search/web",
+    url: "https://dapi.kakao.com/v2/search/image",
     data: { query: search_text, sort: "recency" },
     headers: { Authorization: "KakaoAK 6676411ad5a6389306c0fc9e8fdf2073" }
 })
     .done(function (msg) { // response
-        $( "p" ).append( "<strong>"+msg.documents[0].contents+"</strong>" );
-        $( "p" ).append( "<strong>"+msg.documents[0].url+"</strong>" );
+        for(var i=0; i<5; i++){
+            $( "p" ).append( "<strong>"+msg.documents[i].display_sitename+"</strong> " );
+            $( "p" ).append( "<img src='"+msg.documents[i].thumbnail_url+"'/>" );
+            $("p").append("<br>")
+        }
     });
 }
